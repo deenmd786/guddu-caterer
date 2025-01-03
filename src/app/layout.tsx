a80ed2d7a3347beaabf935a3f52c87ca8f5ece0d
deenmd786 <deen8851@gmail.com>
2025-01-03 11:20:17 +0530

    maid some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,33 @@
-"use client"
+"use client";
 import "./globals.css";
 import Copyright from "../components/layout/CopyRight";
 import Footer from "../components/layout/Footer";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
+import Head from "next/head";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
   return (
     <html lang="en">
+      <Head>
+        <title>Your Site Title</title>
+        <meta name="description" content="Your site description here." />
+        <link rel="icon" href="/favicon.ico" />
+        {/* Add other meta tags as needed */}
+      </Head>
       <body>
-      <Provider store={store}>
+        <Provider store={store}>
           {children}
-          </Provider>
+        </Provider>
         <Footer />
         <Copyright />
+        {/* If you want to use Speed Insights, you can add it here */}
+        <SpeedInsights />
       </body>
     </html>
   );
