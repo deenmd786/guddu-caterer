0703f6ad79c350b84ddc57d3d9e074d4734e761e
deenmd786 <deen8851@gmail.com>
2025-01-05 16:23:27 +0530

    maid changes

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,14 +1,23 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { FaPhone, FaComments, FaCalendarAlt } from "react-icons/fa"; // Importing React Icons
+import { FaPhone, FaComments, FaUtensils } from "react-icons/fa"; // Importing React Icons
 import Button from "../../components/reuseable/Button";
 import Logo from "../../components/reuseable/Logo";
 import EnquiryForm from "../../components/reuseable/CateringForm";
 import Map from "../../components/reuseable/Map";
 
-const ContactUs: React.FC = () => (
-  <div className="bg-white font-LexendDeca text-[var(--text-primary)] pt-24 pb-6">
+const ContactUs: React.FC = () => {
+  const message = "Hello, Guddu Catering Service!";
+
+  const sendMessage = () => {
+    const phoneNumber = "918851253661"; // Fixed phone number
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.open(whatsappURL, "_blank");
+  };
+  return (
+    <div className="bg-white font-LexendDeca text-[var(--text-primary)] pt-24 pb-6">
     <div className="container mx-auto px-4">
       <span className="absolute top-10 left-20">
         <Logo />
@@ -40,43 +49,51 @@ const ContactUs: React.FC = () => (
           </div>
         </div>
 
-        <div className="w-full relative -top-20 px-4 flex flex-col items-center"> {/* Center items */}
-  {/* Adjusting the position of the boxes */}
-  <div className="mt-10 flex flex-col md:flex-row items-center justify-center gap-10 w-full">
-    <div className="bg-[var(--background-secondary)] p-8 rounded-lg text-center w-full md:w-1/3 h-64 flex flex-col justify-between"> {/* Added flex properties */}
-      <div className="my-auto">
-        <FaPhone className="text-gray-500 mb-4 text-3xl mx-auto" />
-        <h2 className="text-xl font-semibold text-[var(--text-primary)] mb-2">
-          Call us directly
-        </h2>
-        <p className="text-2xl font-bold text-[var(--text-primary)] mb-2">
-          09278422964
-        </p>
-        <a className="text-blue-600" href="#">
-          See more local numbers
-        </a>
-      </div>
+        <div className="w-full relative -top-20 px-4 flex flex-col items-center">
+          {" "}
+          {/* Center items */}
+          {/* Adjusting the position of the boxes */}
+          <div className="mt-10 flex flex-col md:flex-row items-center justify-center gap-10 w-full">
+            <div className="bg-[var(--background-secondary)] p-8 rounded-lg text-center w-full md:w-1/3 h-64 flex flex-col justify-between">
+              {" "}
+              {/* Added flex properties */}
+              <a href="tel:09278422964" className="my-auto">
+                <div className="flex flex-col items-center">
+                  <FaPhone className="text-gray-500 mb-4 text-3xl mx-auto" />
+                  <h2 className="text-xl font-semibold text-[var(--text-primary)] mb-2">
+                    Call us directly
+                  </h2>
+                  <p className="text-2xl font-bold text-[var(--text-primary)] mb-2">
+                    09278422964
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div className="bg-[var(--background-secondary)] p-8 rounded-lg text-center w-full md:w-1/3 h-64 flex flex-col justify-between">
+              {" "}
+              {/* Added flex properties */}
+              <div className="my-auto">
+                <FaComments className="text-gray-500 mb-4 mx-auto text-3xl" />
+                <h2 className="text-xl font-semibold text-[var(--text-primary)] mb-2">
+                  Chat with our sales team
+                </h2>
+                <Button label="Chat with Sales" onClick={sendMessage} />
+              </div>
+            </div>
+            <div className="bg-[var(--background-secondary)] p-8 rounded-lg text-center w-full md:w-1/3 h-64 flex flex-col justify-between">
+              {" "}
+              {/* Added flex properties */}
+              <div className="my-auto text-center">
+      <FaUtensils className="text-gray-500 mb-4 mx-auto text-3xl" />
+      <h2 className="text-xl font-semibold text-[var(--text-primary)] mb-2">
+        Book Your Menu Today!
+      </h2>
+      
+      <Button label="Custom Menus" href="/dashboard/book-buffet/menu" />
     </div>
-    <div className="bg-[var(--background-secondary)] p-8 rounded-lg text-center w-full md:w-1/3 h-64 flex flex-col justify-between"> {/* Added flex properties */}
-      <div className="my-auto">
-        <FaComments className="text-gray-500 mb-4 mx-auto text-3xl" />
-        <h2 className="text-xl font-semibold text-[var(--text-primary)] mb-2">
-          Chat with our sales team
-        </h2>
-        <Button label="Chat with Sales" href="#" />
-      </div>
-    </div>
-    <div className="bg-[var(--background-secondary)] p-8 rounded-lg text-center w-full md:w-1/3 h-64 flex flex-col justify-between"> {/* Added flex properties */}
-      <div className="my-auto">
-        <FaCalendarAlt className="text-gray-500 mb-4 mx-auto text-3xl" />
-        <h2 className="text-xl font-semibold text-[var(--text-primary)] mb-2">
-          Get a product demo
-        </h2>
-        <Button label="Get a demo" href="#" />
-      </div>
-    </div>
-  </div>
-</div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="w-full">
         <h1 className="text-[var(--text-red)] text-center text-4xl font-bold mb-8">
@@ -88,13 +105,13 @@ const ContactUs: React.FC = () => (
             <EnquiryForm />
           </div>
           {/* Map Section */}
-          <div className="w-full md:max-w-lg" >
+          <div className="w-full md:max-w-lg">
             <Map />
           </div>
         </div>
       </div>
     </div>
   </div>
-);
+)};
 
 export default ContactUs;
