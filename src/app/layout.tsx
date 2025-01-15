7b68aa75fd01ffc8436e2b6742242c2ac20bfca0
deenmd786 <deen8851@gmail.com>
2025-01-15 13:05:28 +0530

    maid changes in head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,7 @@ import Copyright from "../components/layout/CopyRight";
 import Footer from "../components/layout/Footer";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
-import { DefaultSeo } from "next-seo";
-import defaultSEOConfig from "../../next-seo.config";
-import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 type RootLayoutProps = {
   children: React.ReactNode;
@@ -16,11 +14,31 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <Head>
-        <DefaultSeo {...defaultSEOConfig} />
-      </Head>
+      <head>
+        {/* Default metadata for the entire application */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Guddu Caterer | Best Catering Service in Delhi</title>
+        <meta name="description" content="Exceptional catering services in Delhi." />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:url" content="https://www.gudducaterer.in" />
+        <meta property="og:site_name" content="Guddu Catering" />
+        <meta property="og:title" content="Guddu Caterer | Best Catering Service in Delhi" />
+        <meta property="og:description" content="Exceptional catering services in Delhi." />
+        <meta property="og:image" content="https://www.gudducaterer.in/guddu-catering-service.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Home | Guddu Catering" />
+        <meta name="twitter:description" content="Exceptional catering services in Delhi." />
+        <meta name="twitter:image" content="https://www.gudducaterer.in/guddu-catering-service.jpg" />
+      </head>
       <body>
-        <Provider store={store}>{children}</Provider>
+        {/* Provider ensures Redux state is available globally */}
+        <Provider store={store}>
+          {/* Example of dynamic SEO for specific pages */}
+          <NextSeo title="Home | Guddu Catering" />
+          {children}
+        </Provider>
         <Footer />
         <Copyright />
       </body>
