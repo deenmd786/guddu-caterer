bb81b925464172e7365d74e99f126885b19afb09
deenmd786 <deen8851@gmail.com>
2025-01-20 16:47:23 +0530

    maid change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,34 +3,32 @@
 import "./globals.css";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
+import Head from "next/head";
 
 type RootLayoutProps = {
   children: React.ReactNode;
-  title: string;
-  description: string;
-  image: string;
-  url: string;
 };
 
-export default function RootLayout({
-  children,
-}: RootLayoutProps) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <Head>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Guddu Caterer | Best Catering Services in Delhi with Our Customise Menus.</title>
         <meta name="description" content="Welcome to Guddu Caterer, the best wedding catering service in Delhi. Customized buffets and exceptional live cooking for all occasions." />
-        <meta property="og:title" content="Guddu Caterer | Home" />
-        <meta property="og:description" content="Welcome to Guddu Caterer, the best wedding catering service in Delhi." />
+        <meta property="og:title" content="Guddu Caterer | Best Catering Services in Delhi with Our Customise Menus." />
+        <meta property="og:description" content="Welcome to Guddu Caterer, the best wedding catering service in Delhi. Customized buffets and exceptional live cooking for all occasions." />
         <meta property="og:image" content="https://www.gudducaterer.in/logo.png" />
         <meta property="og:url" content="https://www.gudducaterer.in/" />
         <meta property="og:type" content="website" />
       </head>
+      </Head>
       <body>
         <Provider store={store}>{children}</Provider>
       </body>
     </html>
   );
+
 }
